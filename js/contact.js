459d777a9d79117d9f62e78dfaba13cb717c0fcf
pretzL <ericpretzinger@gmail.com>
2022-06-06 23:47:09 +0200

    Added contact form and posting to WP

---
+++ js/contact.js
@@ -0,0 +1,74 @@
+const form = document.querySelector(".contact-form");
+
+const userName = document.querySelector("#your-name");
+const userNameError = document.querySelector("#your-name-error");
+
+const emailForm = document.querySelector("#your-email");
+const emailError = document.querySelector("#email-error");
+
+const subjectForm = document.querySelector("#your-subject");
+const subjectError = document.querySelector("#subject-error");
+
+const messageForm = document.querySelector("#your-message");
+const messageError = document.querySelector("#message-error");
+
+const validatorContainer = document.querySelector(".validator-container");
+
+function validateForm(evt) {
+  evt.preventDefault();
+  // Form validation
+  if (checkLength(userName.value, 5)) {
+    userNameError.style.display = "none";
+  } else {
+    userNameError.style.display = "block";
+  }
+
+  if (validateEmail(emailForm.value)) {
+    emailError.style.display = "none";
+  } else {
+    emailError.style.display = "block";
+  }
+
+  if (checkLength(subjectForm.value, 15)) {
+    subjectError.style.display = "none";
+  } else {
+    subjectError.style.display = "block";
+  }
+
+  if (checkLength(messageForm.value, 25)) {
+    messageError.style.display = "none";
+  } else {
+    messageError.style.display = "block";
+  }
+
+  // Form validated message
+  if (checkLength(userName.value, 5) && validateEmail(emailForm.value) && checkLength(subjectForm.value, 15) && checkLength(messageForm.value, 25)) {
+    validatorContainer.style.display = "block";
+
+    // Passing the form contents to Contact Form 7 Plugin and storing it in Flamingo, but only when the form is validated
+    const formElement = evt.target;
+
+    let dataObj = new FormData(formElement);
+
+    const contactURL = "https://pretzl.one/foodforthought/wp-json/contact-form-7/v1/contact-forms/181/feedback";
+    fetch(contactURL, {
+      method: "POST",
+      body: dataObj,
+    })
+      .then((response) => response.json())
+      .catch((error) => console.log("error", error));
+  }
+}
+
+form.addEventListener("submit", validateForm);
+// Input length check
+function checkLength(value, char) {
+  return value.trim().length > char;
+}
+
+// Check email, taken from video "Simple form validation" from Noroff JS1 Module 4 lesson 4.
+function validateEmail(email) {
+  const regEx = /\S+@\S+\.\S+/;
+  const patternMatches = regEx.test(email);
+  return patternMatches;
+}

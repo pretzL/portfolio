4999856d11a9e934f42358f9b7da5573c4087ad1
pretzL <ericpretzinger@gmail.com>
2022-06-07 12:07:01 +0200

    Added hover state to display tooltip for social icons

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
+const iconButton = document.querySelectorAll(".social-icons");
 
+iconButton.forEach((e) => {
+  e.addEventListener("mouseover", () => {
+    const toolTip = e.firstElementChild;
+    toolTip.style.display = "block";
+  });
+});
+
+iconButton.forEach((e) => {
+  e.addEventListener("mouseout", () => {
+    const toolTip = e.firstElementChild;
+    toolTip.style.display = "none";
+  });
+});

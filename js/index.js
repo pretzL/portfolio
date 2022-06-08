05f62f7831842ea8b49caa6db00301e07fa76b06
pretzL <ericpretzinger@gmail.com>
2022-06-08 23:20:50 +0200

    Added carousel to last card project display

--- js/index.js
+++ js/index.js
@@ -15,3 +15,42 @@ iconButton.forEach((e) => {
     toolTip.style.display = "none";
   });
 });
+
+// CAROUSEL FROM https://www.youtube.com/watch?v=yq4BeRtUHbk
+
+// Handle button for carousel sliding
+document.addEventListener("click", (e) => {
+  let carouselButton;
+  if (e.target.matches(".arrow")) {
+    carouselButton = e.target;
+  } else {
+    carouselButton = e.target.closest(".arrow");
+  }
+
+  if (carouselButton != null) {
+    moveCarousel(carouselButton);
+  }
+});
+
+// Carousel slide function, with infinite loop
+function moveCarousel(button) {
+  const carouselContainer = document.querySelector(".all-projects-container");
+  const carouselIndex = parseInt(getComputedStyle(carouselContainer).getPropertyValue("--carousel-index"));
+  const itemsPerScreen = parseInt(getComputedStyle(carouselContainer).getPropertyValue("--items-per-screen"));
+
+  if (button.classList.contains("left-arrow")) {
+    if (carouselIndex - 1 < 0) {
+      carouselContainer.style.setProperty("--carousel-index", 4 / itemsPerScreen - 1);
+    } else {
+      carouselContainer.style.setProperty("--carousel-index", carouselIndex - 1);
+    }
+  }
+
+  if (button.classList.contains("right-arrow")) {
+    if (carouselIndex + 1 >= 4 / itemsPerScreen) {
+      carouselContainer.style.setProperty("--carousel-index", 0);
+    } else {
+      carouselContainer.style.setProperty("--carousel-index", carouselIndex + 1);
+    }
+  }
+}

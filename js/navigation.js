4a0c9a0039d3dd16cbbc8f19bba38ddb7ea02de2
Eric Pretzinger <ericpretzinger@gmail.com>
2025-11-12 10:22:27 +0100

    Cleanup on aisle 3

--- js/navigation.js
+++
@@ -1,9 +0,0 @@
-// NAVBAR
-
-const label = document.querySelector(".menu-icon");
-const navbar = document.querySelector(".nav-menu");
-
-// When clicking the hamburger icon
-label.addEventListener("click", () => {
-  navbar.classList.toggle("hamburger-active");
-});

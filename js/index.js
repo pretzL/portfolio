63bea4450975ed78849584e04720f5cc6b7dd9ff
Eric Pretzinger <ericpretzinger@gmail.com>
2025-11-12 11:21:00 +0100

    Add experience counter

---
+++ js/index.js
@@ -0,0 +1,6 @@
+// Experience timer
+const experienceEl = document.querySelector("#experience-time");
+const currentYear = new Date().getFullYear();
+const startYear = 2021;
+const yearsOfExperience = currentYear - startYear;
+experienceEl.textContent = yearsOfExperience;

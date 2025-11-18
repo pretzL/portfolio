7c681ff5a08a4b540930ea18c2f6c811bd27eac6
Eric Pretzinger <ericpretzinger@gmail.com>
2025-11-18 07:03:05 +0100

    Cleanup

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 // Experience timer
 const experienceEl = document.querySelector("#experience-time");
+
 const currentYear = new Date().getFullYear();
 const startYear = 2021;
 const yearsOfExperience = currentYear - startYear;
+
 experienceEl.textContent = yearsOfExperience;
